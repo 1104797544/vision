8712542729e3d2da20be47973864e8e460788fa5
1104797544 <1104797544@qq.com>
2020-06-21 01:30:44 +0800

    oom

---
+++ src/main/java/com/jzf/vm/HeapOOM.java
@@ -0,0 +1,24 @@
+package com.jzf.vm;
+
+import com.jzf.App;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOOM {
+
+    static class OOMObject {
+
+    }
+
+    public static void main( String[] args )
+    {
+        List<OOMObject> oomObjectList = new ArrayList<>();
+
+        while (true) {
+            oomObjectList.add(new OOMObject());
+            System.out.println(oomObjectList.size());
+        }
+    }
+
+}
---
+++ src/main/java/com/jzf/vm/RuntimeConstantPoolOOM.java
@@ -0,0 +1,18 @@
+package com.jzf.vm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuntimeConstantPoolOOM {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (true) {
+            list.add(String.valueOf(i).intern());
+            System.out.println(list.size());
+        }
+
+    }
+
+}
